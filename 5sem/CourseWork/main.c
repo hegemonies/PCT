bdc706f0be9381dcfacfb94c61387d889e3d35f8
hegemonies <hegemonies@yandex.ru>
2018-12-02 22:15:06 +0700

    Fix

--- 5sem/CourseWork/main.c
+++ 5sem/CourseWork/main.c
@@ -4,13 +4,24 @@
 #include <limits.h>
 #include <time.h>
 
-#define INF_PERC 40
+#define INF_PERC 30
 
 static int rank;
+static int commsize;
 
 int min(int a, int b) {
     return (a < b) ? a : b;
 }
+
+int Min(int A, int B) {
+    int Result = (A < B) ? A : B;
+
+    if((A < 0) && (B >= 0)) Result = B;
+    if((B < 0) && (A >= 0)) Result = A;
+    if((A < 0) && (B < 0)) Result = -1;
+
+    return Result;
+} 
 
 void data_random_init(int *arr, int n) {
     srand(time(0));
@@ -41,7 +52,7 @@ void dummy_data_init(int *arr, int n) {
             } else if (i == 0) {
                 arr[i * n + j] = j;
             } else {
-                arr[i * n + j] = -1;
+                arr[i * n + j] = INT_MAX;
             }
             arr[j * n + i] = arr[i * n + j];
         }
@@ -59,6 +70,18 @@ void serial_Floyd(int *arr, int n) {
         }
     }
 }
+
+void SerialFloyd(int *arr, int n) {
+    int t1, t2;
+    for(int k = 0; k < n; k++)
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                if((arr[i * n + k] != INT_MAX) && (arr[k * n + j] != INT_MAX)) {
+                    t1 = arr[i * n + j];
+                    t2 = arr[i * n + k] + arr[k * n + j];
+                    arr[i * n + j] = min(t1, t2);
+                }
+} 
 
 void print_matrix(int *arr, int n) {
     for (int i = 0; i < n; i++) {
@@ -103,26 +126,80 @@ void copy(int *start, int n, int *out) {
 }
 
 void row_distr(int *arr, int n, int count_rows, int k, int *row) {
-    int row_rank = k / count_rows;
-    int row_num = k - row_rank * count_rows;
+    int row_rank;
+    int rest_rows = n;
+    int index = 0;
+    int num = n / commsize;
+
+    for (row_rank = 1; row_rank < commsize + 1; row_rank++) {
+        if (k < index + num) {
+            break;
+        }
+        rest_rows -= num;
+        index += num;
+        num = rest_rows / (commsize - row_rank);
+    }
+    row_rank = row_rank - 1;
+    int row_num = k - index;
 
     if (row_rank == rank) {
         copy(&arr[row_num * n], n, row);
     }
-
-    MPI_Bcast(row, n, MPI_INT, row_rank, MPI_COMM_WORLD); 
+    
+    MPI_Bcast(row, n, MPI_INT, row_rank, MPI_COMM_WORLD);
 }
+
+void RowDistribution(int *pProcRows, int Size, int RowNum, int k, int *pRow) {
+    int ProcRowRank; 
+    int ProcRowNum; 
+
+    int RestRows = Size;
+    int Ind = 0;
+    int Num = Size / commsize;
+
+    for(ProcRowRank = 1; ProcRowRank < commsize + 1; ProcRowRank ++) {
+        if(k < Ind + Num ) 
+            break;
+        RestRows -= Num;
+        Ind += Num;
+        Num = RestRows / (commsize - ProcRowRank);
+    }
+    ProcRowRank = ProcRowRank - 1;
+    ProcRowNum = k - Ind;
+    if(ProcRowRank == rank)
+        copy(&pProcRows[ProcRowNum * Size], Size, pRow);
+        
+    MPI_Bcast(pRow, Size, MPI_INT, ProcRowRank, MPI_COMM_WORLD);
+} 
 
 void par_Floyd(int *arr, int n, int count_rows) {
     int *row = calloc(sizeof(int), n);
 
     for (int k = 0; k < n; k++) {
-        row_distr(arr, n, count_rows, k, row);
+        // row_distr(arr, n, count_rows, k, row);
+        RowDistribution(arr, n, count_rows, k, row);
+
+        // for (int p = 0; p < commsize; p++) {
+        //     if (p == rank) {
+        //         printf("rank = %d rr = ", rank);
+        //         for (int i = 0; i < n; i++) {
+        //             if (row[i] == INT_MAX) {
+        //                 printf("INF ");
+        //             } else {
+        //                 printf("%d\t\t", row[i]);
+        //             }
+        //         }
+        //         printf("\n");
+        //     }
+        //     MPI_Barrier(MPI_COMM_WORLD);
+        // }
 
         for (int i = 0; i < count_rows; i++) {
-            for (int j = 0; j < n; j++) {
-                if ((arr[i * n + j] != INT_MAX) && (row[j] != INT_MAX)) {
-                    arr[i * n + j] = min(arr[i * n + j], arr[i * n + k] + row[j]);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if ((arr[i * n + k] != INT_MAX) && (row[j] != INT_MAX)) {
+                        arr[i * n + j] = min(arr[i * n + j], arr[i * n + k] + row[j]);
+                    }
                 }
             }
         }
@@ -135,10 +212,9 @@ int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
 	
 	// int rank;
-	int commsize;
+	// int commsize;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &commsize);
-
 
     int n;
     int rem;
@@ -162,8 +238,22 @@ int main(int argc, char **argv) {
         // data_random_init(arr, n);
         dummy_data_init(arr, n);
 
+        printf("arr:\n");
         print_matrix(arr, n);
+
+        int *cp_arr = calloc(sizeof(int), n * n);
+        copy(arr, n * n, cp_arr);
+
+        // printf("\nbefore floyd cp_arr:\n");
+        // print_matrix(cp_arr, n);
+
+        serial_Floyd(cp_arr, n);
+        // SerialFloyd(cp_arr, n);
+
+        printf("\nafter floyd cp_arr:\n");
+        print_matrix(cp_arr, n);
     }
+    MPI_Barrier(MPI_COMM_WORLD);
 
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
@@ -179,35 +269,48 @@ int main(int argc, char **argv) {
     int *sendsize = calloc(sizeof(int), commsize);
     int *displs = calloc(sizeof(int), commsize);
 
+    int f = 0;
+    if (rem > 0) {
+        f = -1;
+    }
+
     for (int i = 0; i < commsize; i++) {
         sendsize[i] = (n * n) / commsize;
+        sendsize[i] += f;
 
         if (rem > 0) {
             sendsize[i] += n;
             rem--;
-            sendsize[i]--;
+            // sendsize[i]--;
         }
 
         displs[i] = (i > 0) ? displs[i - 1] + sendsize[i - 1] : 0;
-        if (rank == 0) {
-            // printf("sendsize[%d] = %d\n", i, sendsize[i]);
-            // printf("displs[%d] = %d\n", i, displs[i]);
-        }
+        // if (rank == 0) {
+        //     printf("sendsize[%d] = %d\n", i, sendsize[i]);
+        //     printf("displs[%d] = %d\n", i, displs[i]);
+        // }
     }
 
     MPI_Scatterv(arr, sendsize, displs, MPI_INT, recv_arr, n * count_rows, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // MPI_Barrier(MPI_COMM_WORLD);
-    // par_print_matrix(recv_arr, n, count_rows, rank, commsize);
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (rank == 0) {
+        printf("\nprint before par_Floyd\n");
+    }
+    par_print_matrix(recv_arr, n, count_rows, rank, commsize);
     printf("\n");
     // if (rank == 0) {
     //     print_matrix(arr, n);
     // }
 
-    par_Floyd(arr, n, count_rows);
+    MPI_Barrier(MPI_COMM_WORLD);
+    par_Floyd(recv_arr, n, count_rows);
 
-    // MPI_Barrier(MPI_COMM_WORLD);
-    // par_print_matrix(recv_arr, n, count_rows, rank, commsize);
+    if (rank == 0) {
+        printf("\nprint after par_Floyd\n");
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    par_print_matrix(recv_arr, n, count_rows, rank, commsize);
 
     int *recv_num = calloc(sizeof(int), commsize);
     int *recv_index = calloc(sizeof(int), commsize);
@@ -224,7 +327,9 @@ int main(int argc, char **argv) {
     MPI_Gatherv(recv_arr, recv_num[rank], MPI_INT, 
     arr, recv_num, recv_index, MPI_INT, 0, MPI_COMM_WORLD);
 
+    MPI_Barrier(MPI_COMM_WORLD);
     if (rank == 0) {
+        printf("\nprint ser after gather\n");
         print_matrix(arr, n);
     }
 
